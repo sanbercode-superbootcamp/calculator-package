626b1a0d4d503d7b0df336fa4e082929e2b4a1cb
ardhihdra <ardhi.rofi@gmail.com>
2019-10-02 23:16:15 +0700

    tugas2 ardhi

---
+++ package/node_modules/ardhi-calculator/src/index.js
@@ -0,0 +1,16 @@
+var calculators = {
+  plus: function(a, b) {
+    return Number(a) + Number(b);
+  },
+  minus: function(a, b) {
+    return Number(a) - Number(b);
+  },
+  times: function(a, b) {
+    return Number(a) * Number(b);
+  },
+  divide: function(a, b) {
+    return Number(a) / Number(b);
+  }
+}
+
+module.exports = calculators;
