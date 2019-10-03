cc98aba19d2467c0b553b72f98230c98b0628f17
satriahdyt <satriahdyt@users.noreply.github.com>
2019-10-03 09:22:32 +0700

    Tugas D-2

---
+++ src/calculatorlib.js
@@ -0,0 +1,15 @@
+exports.plus = function(a, b) {
+    return a + b;
+}
+
+exports.minus = function(a, b) {
+    return a - b;
+}
+
+exports.times = function(a, b) {
+    return a * b;
+}
+
+exports.divide = function(a, b) {
+    return a / b;
+}
