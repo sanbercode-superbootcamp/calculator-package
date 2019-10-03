755ac4efd9b508f89925d9cedf4224a2b1761a52
febry55 <febryanata14@student.unsil.ac.id>
2019-10-03 07:52:56 +0700

    commit tugas-hari-2

---
+++ Tugas-hari-2/use-calc-day2/index.js
@@ -0,0 +1,6 @@
+const calc = require('calc-day2');
+console.log("Hasil pertambahan : " + calc.addition(3, 5));
+console.log("Hasil pengurangan : " + calc.subtraction(5, 3));
+console.log("Hasil perkalian : " + calc.multiplication(3, 5));
+console.log("Hasil pembagian : " + calc.division(10, 5));
+console.log("Hasil modulus : " + calc.modulus(6, 5));
---
+++ Tugas-hari-2/use-calc-day2/node_modules/calc-day2/src/calculator.js
@@ -0,0 +1,20 @@
+const calculator = {
+    addition: function (num1, num2) {
+        return num1 + num2;
+    },
+    subtraction: function (num1, num2) {
+        return num1 - num2;
+    },
+    division: function (num1, num2) {
+        return num1 / num2;
+    },
+    multiplication: function (num1, num2) {
+        return num1 * num2;
+    },
+    modulus: function (num1, num2) {
+        return num1 % num2;
+    }
+}
+
+
+module.exports = calculator;
